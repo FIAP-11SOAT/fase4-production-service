8eb2274379360a7494edd59ddd68746adb150b4c
Matheus Ribeiro <teteuoliveira12@gmail.com>
2025-12-22 21:34:17 -0300

    feat(terraform): Atualizar nome da tabela DynamoDB para consistência
    - Alterado o nome da tabela DynamoDB de "${local.project_name}-production" para "${local.project_name}-table" no arquivo dynamodb.tf.
    - Atualizado o valor da tag "Name" para "${local.project_name}-table" para refletir a mudança no nome da tabela.
    
    Essas alterações garantem que o nome da tabela DynamoDB seja consistente com a nomenclatura utilizada em outras partes do projeto, melhorando a clareza e a manutenção do código.

--- deploy/terraform/dynamodb.tf
+++ deploy/terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "production" {
-  name         = "${local.project_name}-production"
+  name         = "${local.project_name}-table"
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "id"
@@ -10,6 +10,6 @@ resource "aws_dynamodb_table" "production" {
   }
 
   tags = {
-    Name        = "${local.project_name}-production"
+    Name = "${local.project_name}-table"
   }
 }
