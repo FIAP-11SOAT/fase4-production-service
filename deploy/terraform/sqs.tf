f0b0ad5e7b2d0d0fb6ba3c1edca6cb36e529d0eb
Matheus Ribeiro <teteuoliveira12@gmail.com>
2025-12-23 06:10:45 -0300

    feat(terraform): Remover fila SQS de pedidos do arquivo sqs.tf
    - Removida a definição da fila SQS "order_queue" do arquivo sqs.tf.
    - Mantida a fila SQS "production_queue" com suas configurações.
    
    Essa alteração simplifica a configuração do Terraform, eliminando a fila SQS desnecessária para o serviço de pedidos, o que melhora a clareza e a manutenção do código. A remoção contribui para um gerenciamento mais eficiente dos recursos SQS no projeto.

--- deploy/terraform/sqs.tf
+++ deploy/terraform/sqs.tf
@@ -12,18 +12,3 @@ resource "aws_sqs_queue" "production_queue" {
   }
 }
 
-
-
-resource "aws_sqs_queue" "order_queue" {
-  name = "fase4-order-service-queue"
-
-  visibility_timeout_seconds = 30
-  message_retention_seconds  = 345600 # 4 dias
-  max_message_size           = 262144 # 256 KB
-  delay_seconds              = 0
-  receive_wait_time_seconds  = 5
-
-  tags = {
-    Name = "fase4-order-service-queue"
-  }
-}
